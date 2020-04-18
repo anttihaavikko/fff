743f0b259550b6850a105167942ac8bb0c7d76dd
Antti Haavikko <antti.haavikko@focus.fi>
2020-04-18 22:08:01 +0300

    Add lurkers

---
+++ Assets/Scripts/Lurker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lurker : MonoBehaviour
+{
+    public List<SpriteRenderer> blushes;
+    public Color altColor;
+    public Face face;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale *= Random.Range(0.75f, 1.2f);
+
+        if(Random.value < 0.5f)
+        {
+            blushes.ForEach(b => b.color = altColor);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit by " + collision.gameObject.name);
+
+        if(collision.gameObject.tag == "Poop")
+        {
+            face.Emote(Face.Emotion.Sad, Face.Emotion.Default, 2f);
+        }
+
+        if (collision.gameObject.tag == "Bird")
+        {
+            face.Emote(Face.Emotion.Shocked, Face.Emotion.Default, 3f);
+        }
+    }
+}
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -409,7 +409,8 @@ public class MeshGenerator : MonoBehaviour {
         if (Random.value < 0.4f)
         {
             var decoration = decorations[Random.Range(0, decorations.Count)];
-            var d = Instantiate(decoration, new Vector3(pos.x, pos.y, -2.5f), Quaternion.Euler(Vector3.zero));
+            var d = Instantiate(decoration);
+            d.transform.position = new Vector3(pos.x, pos.y, d.position.z);
             d.transform.localScale *= Random.Range(0.8f, 1.2f);
             d.transform.parent = transform;
         }
